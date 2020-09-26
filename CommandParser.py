af0cca2305a97a00f7d31d43ebeb25843310f708
danieldanieltata <danieltalor1@gmail.com>
2020-09-26 19:21:39 +0300

    Delete CommandParser.py

--- CommandParser.py
+++
@@ -1,7 +0,0 @@
-import Server
-import json
-
-def getCommands(Server): #Parses the commands and puts it in a dictionary
-	Data = Server.getPackets()
-	commands_in_dict = json.loads(Data)
-	return commands_in_dict
